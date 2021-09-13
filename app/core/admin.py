b60c0d0a8c470ae9f21b7937e606ab556b34ef02
Robert Durica <rdurica@gmail.com>
2021-09-13 20:57:03 +0200

    first commit

---
+++ app/core/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+"""
+Example of module registration
+
+class ApplicationTypeAdmin(admin.ModelAdmin):
+    list_display = ('some_field',)
+    list_display_links = ('some_field',)
+    search_fields = ('some_field',)
+    list_per_page = 50
+
+
+admin.site.register(LegalReason, LegalReasonAdmin)
+
+"""
